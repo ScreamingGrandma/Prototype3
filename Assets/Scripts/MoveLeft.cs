3841238b2175d714104c254cc0de418f60e7bd32
ScreamingGrandma <Keanu.Ngov@Gmail.com>
2025-08-30 13:54:25 -0700

    Add: obstacle movement

---
+++ Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    public float speed = 20f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
+    }
+}
