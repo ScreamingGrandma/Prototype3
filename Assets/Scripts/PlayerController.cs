e8c4c2b6f049e9cc3aa29cd7fe95633ea6cf109b
ScreamingGrandma <Keanu.Ngov@Gmail.com>
2025-09-20 13:46:43 -0700

    Add: sound effects

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -18,12 +18,19 @@ public class PlayerController : MonoBehaviour
     public ParticleSystem dirtParticle;
     public ParticleSystem explosionParticle;
 
+    public AudioClip crashSound;
+    public AudioClip jumpSound;
+
+    public AudioSource playerAudio;
+
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
         Physics.gravity *= gravityModifier;
         playerAnim = GetComponent<Animator>();
+
+        playerAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -35,6 +42,8 @@ public class PlayerController : MonoBehaviour
             isOnGround = false;
             playerAnim.SetTrigger("Jump_trig");
             dirtParticle.Stop();
+
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
         }
     }
 
@@ -47,16 +56,27 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
+
+            if (!gameOver)
             dirtParticle.Play();
         }
         else if (collision.gameObject.CompareTag("Obstacle"))
         {
+
+            if (!gameOver)
+            {
+                playerAudio.PlayOneShot(crashSound, 1.0f);
+                explosionParticle.Play();
+            }
+           
+
             gameOver = true;
             Debug.Log("Game Over!");
             playerAnim.SetBool("Death_b", true);
             playerAnim.SetInteger("DeathType_int", 1);
-            explosionParticle.Play();
             dirtParticle.Stop();
+
+          
         }
     }
 }
